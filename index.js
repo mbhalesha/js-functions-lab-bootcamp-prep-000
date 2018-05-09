ca8a94ee845b75a3f82c993b70a44d074a128f33
Manisha Bhalesha <mbhalesha@gmail.com>
2018-05-09 23:52:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 function happyHolidays
 return "Happy holiday!"
 
+function happyHolidaysTo
+return `Happy holidays, ${name}!`
+
+function happyHolidayTo
+return `Happy ${holiday}, ${name}!`
+
+function holidayCountdown
+return `IT's ${days} days until ${holiday}!`
